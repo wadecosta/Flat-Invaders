c87f5fee337773c9b8a4a5cf3ce1be6e261b222a
wadecosta <wadeccosta@gmail.com>
2022-03-07 22:20:05 -0800

    C# is too sharp

--- src/Assets/Scripts/EnemyMovement.cs
+++ src/Assets/Scripts/EnemyMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public int count;
+    //private float time = 5f;
+    public float period = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+	    count = 0;
 
         
     }
@@ -14,8 +19,48 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	    float x = transform.position.x;
-	    Debug.Log(x);
+
+	    if((period > 2) && (count < 5))
+	    {
+
+     	        float x = transform.position.x;
+                float y = transform.position.y;
+
+                transform.position = new Vector3(x+2, y, 0);
+                count++;
+
+		period = 0;
+
+
+	    }
+
+	    if((period > 2) && (count == 5))
+	    {
+		    float x = transform.position.x;
+                    float y = transform.position.y;
+
+		    transform.position = new Vector3(x, y-1, 0);
+		    count++;
+
+		    period = 0;
+	    }
+
+	    if((period > 2) && (count > 5))  
+            {       
+                
+                float x = transform.position.x;
+                float y = transform.position.y;             
+
+                transform.position = new Vector3(x-2, y, 0);
+                count++;
+                
+                period = 0;
+                    
+
+            }
+
+
+	    period += Time.deltaTime;
         
     }
 }
