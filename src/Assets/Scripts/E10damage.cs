9a93df828cdf015d3dd179131dc47add065aa37e
Wade Costa <wadeccosta@gmail.com>
2022-03-14 19:23:24 -0700

    Added some Animations

--- src/Assets/Scripts/E10damage.cs
+++ src/Assets/Scripts/E10damage.cs
@@ -4,21 +4,54 @@ using UnityEngine;
 
 public class E10damage : MonoBehaviour
 {
+    public GameObject bullet;
+    public Transform shottingOffset;
+
+    public float time = 0.0f;
+    public float speed = 5;
+
+    public Rigidbody2D shot;
+
+    public Animator explosion;
+
+    public string explosionString = "BOOM!";
+
+
     // Start is called before the first frame update
     void Start()
     {
+	    time = 0;
+	    //shot = GetComponent<Rigidbody2D>();
+	    //Shoot();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+	    time += Time.deltaTime;
+
+	    if(time > 5)
+	    {
+		    //GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+            	    //Destroy(shot, 3f);
+		    time = 0;
+	    }
+	    //GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+	    //Destroy(shot, 3f);
         
     }
+
+    //private void Shoot()
+    //{
+//	    shot = Vector2.down * speed;
+  //  }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
             Score.score += 10;
             Destroy(this.gameObject);
+
+	    //explosion.Play(explosion.String, 0, 0.0f);
     }
 }
--- src/Assets/Scripts/Player.cs
+++ src/Assets/Scripts/Player.cs
@@ -8,12 +8,22 @@ public class Player : MonoBehaviour
 
   public float speed = 5f;
 
+  public AudioSource bulletSound;
+
   public Transform shottingOffset;
+
+    void Start()
+    {
+	    bulletSound = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void Update()
     {
       if (Input.GetKeyDown(KeyCode.Space))
       {
+	bulletSound = GetComponent<AudioSource>();
+	bulletSound.Play();
+
         GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
 
         Destroy(shot, 3f);
